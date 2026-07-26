407322cd78a7a49b5588583ecb009f0b5663e9b0
navketanv <71421842+navketanv@users.noreply.github.com>
2026-07-27 00:53:20 +0200

    cleaned-up BufferStorage.tpp and Allocator.tpp

--- include/Allocator.h
+++ include/Allocator.h
@@ -1,7 +1,9 @@
 #pragma once
 #include <cstddef>
-#include <utility>
+#include <limits>
+#include <new>
 #include <type_traits>
+#include <utility>
 
 template<typename T>
 class Allocator;
@@ -27,13 +29,52 @@ public:
     template<typename U>
     Allocator(const Allocator<U>& rhs) noexcept {}
 
-    [[nodiscard]] T* allocate(std::size_t count);
-    void deallocate(T* ptr, std::size_t count) noexcept;
+    [[nodiscard]] T* allocate(std::size_t count) const;
+    void deallocate(T* ptr, std::size_t count) const noexcept;
 
     template<typename... Args>
-    void construct(T* ptr, Args&&... args);
-    void destroy(T* ptr) noexcept;
+    void construct(T* ptr, Args&&... args) const;
+    void destroy(T* ptr) const noexcept;
     std::size_t max_size() const noexcept;
 };
 
-#include "Allocator.tpp"
+template<typename T>
+T* Allocator<T>::allocate(std::size_t count) const {
+    if (count == 0) {
+        return nullptr;
+    }
+
+    if (count > Allocator<T>::max_size()) {
+        throw std::bad_array_new_length();
+    }
+
+    return static_cast<T*>(::operator new(sizeof(T) * count));
+}
+
+template<typename T>
+void Allocator<T>::deallocate(T* ptr, std::size_t count) const noexcept {
+    if (ptr != nullptr) {
+        ::operator delete(ptr, (sizeof(T) * count));
+        ptr = nullptr;
+    }
+}
+
+template<typename T>
+template<typename... Args>
+void Allocator<T>::construct(T* ptr, Args&&... args) const {
+    if (ptr != nullptr) {
+        ::new (static_cast<void*>(ptr)) T(std::forward<Args>(args)...);
+    }
+}
+
+template<typename T>
+void Allocator<T>::destroy(T* ptr) const noexcept {
+    if (ptr != nullptr) {
+        ptr->~T();
+    }
+}
+
+template<typename T>
+std::size_t Allocator<T>::max_size() const noexcept {
+    return std::numeric_limits<std::size_t>::max() / sizeof(T);
+}
--- include/BufferStorage.h
+++ include/BufferStorage.h
@@ -4,6 +4,7 @@
 #include <cstddef>
 #include <memory>
 #include <type_traits>
+#include <utility>
 
 template<typename T, typename Alloc = Allocator<T>>
 class BufferStorage
@@ -66,4 +67,95 @@ void swap(BufferStorage<T, Alloc>& lhs,
     lhs.swap(rhs);
 }
 
-#include "BufferStorage.tpp"
+template<typename T, typename Alloc>
+BufferStorage<T, Alloc>::BufferStorage(BufferStorage<T, Alloc>::size_type capacity)
+    : m_allocator{}
+    , m_pData(capacity ? BufferStorage<T, Alloc>::AllocatorTraits::allocate(m_allocator, capacity) : nullptr)
+    , m_capacity(capacity) {}
+
+template<typename T, typename Alloc>
+BufferStorage<T, Alloc>::BufferStorage(const BufferStorage<T, Alloc>::allocator_type& alloc, BufferStorage<T, Alloc>::size_type capacity)
+    : m_allocator(alloc)
+    , m_pData(capacity ? BufferStorage<T, Alloc>::AllocatorTraits::allocate(m_allocator, capacity) : nullptr)
+    , m_capacity(capacity) {}
+
+template<typename T, typename Alloc>
+BufferStorage<T, Alloc>::BufferStorage(BufferStorage<T, Alloc>&& rhs) noexcept(std::is_nothrow_move_constructible_v<Alloc>)
+    : m_allocator(std::move(rhs.m_allocator))
+    , m_pData(std::exchange(rhs.m_pData, nullptr))
+    , m_capacity(std::exchange(rhs.m_capacity, 0)) {}
+
+template<typename T, typename Alloc>
+BufferStorage<T, Alloc>& BufferStorage<T, Alloc>::operator=(BufferStorage<T, Alloc>&& rhs) noexcept(std::is_nothrow_move_assignable_v<Alloc>) {
+    if (this != &rhs) {
+        releaseStorage();
+        m_allocator = std::move(rhs.m_allocator);
+        m_pData = std::exchange(rhs.m_pData, nullptr);
+        m_capacity = std::exchange(rhs.m_capacity, 0);
+    }
+    return *this;
+}
+
+template<typename T, typename Alloc>
+BufferStorage<T, Alloc>::~BufferStorage() {
+    releaseStorage();
+}
+
+template<typename T, typename Alloc>
+void BufferStorage<T, Alloc>::swap(BufferStorage<T, Alloc>& rhs) noexcept(kStorageSwapNoexcept) {
+    using std::swap;
+    swap(m_allocator, rhs.m_allocator);
+    swap(m_pData, rhs.m_pData);
+    swap(m_capacity, rhs.m_capacity);
+}
+
+template<typename T, typename Alloc>
+void BufferStorage<T, Alloc>::stealStorage(BufferStorage<T, Alloc>& rhs) noexcept {
+    releaseStorage();
+    m_pData = std::exchange(rhs.m_pData, nullptr);
+    m_capacity = std::exchange(rhs.m_capacity, 0);
+}
+
+template<typename T, typename Alloc>
+typename BufferStorage<T, Alloc>::pointer
+BufferStorage<T, Alloc>::data() noexcept {
+    return m_pData;
+}
+
+template<typename T, typename Alloc>
+typename BufferStorage<T, Alloc>::const_pointer
+BufferStorage<T, Alloc>::data() const noexcept {
+    return m_pData;
+}
+
+template<typename T, typename Alloc>
+typename BufferStorage<T, Alloc>::allocator_type&
+BufferStorage<T, Alloc>::allocator() noexcept {
+    return m_allocator;
+}
+
+template<typename T, typename Alloc>
+const typename BufferStorage<T, Alloc>::allocator_type&
+BufferStorage<T, Alloc>::allocator() const noexcept {
+    return m_allocator;
+}
+
+template<typename T, typename Alloc>
+typename BufferStorage<T, Alloc>::size_type
+BufferStorage<T, Alloc>::capacity() const noexcept {
+    return m_capacity;
+}
+
+template<typename T, typename Alloc>
+bool BufferStorage<T, Alloc>::hasStorage() const noexcept {
+    return m_pData != nullptr;
+}
+
+template<typename T, typename Alloc>
+void BufferStorage<T, Alloc>::releaseStorage() noexcept {
+    if (m_pData != nullptr) {
+        BufferStorage<T, Alloc>::AllocatorTraits::deallocate(m_allocator, m_pData, m_capacity);
+        m_pData = nullptr;
+        m_capacity = 0;
+    }
+}
