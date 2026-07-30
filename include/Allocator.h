46419f3cec2141b0c60a01e21cbb786418e879c8
navketanv <71421842+navketanv@users.noreply.github.com>
2026-07-30 10:17:02 +0200

    rebind added to Allocator and StatefulAllocator

--- include/Allocator.h
+++ include/Allocator.h
@@ -9,13 +9,17 @@ template<typename T>
 class Allocator;
 
 template<typename T, typename U>
-bool operator==(const Allocator<T>& lhs, const Allocator<U>& rhs) noexcept {
+constexpr bool operator==(const Allocator<T>& lhs,
+                          const Allocator<U>& rhs) noexcept
+{
     return true;
 }
 
 template<typename T, typename U>
-bool operator!=(const Allocator<T>& lhs, const Allocator<U>& rhs) {
-    return false;
+constexpr bool operator!=(const Allocator<T>& lhs,
+                          const Allocator<U>& rhs)
+{
+    return !(lhs == rhs);
 }
 
 template<typename T>
@@ -23,31 +27,44 @@ class Allocator
 {
 public:
     using value_type = T;
-    using is_always_equal = std::true_type;
+
+    template<typename U>
+    struct rebind {
+        using other = Allocator<U>;
+    };
+
+    using is_always_equal = std::false_type;
     using propagate_on_container_copy_assignment = std::false_type;
     using propagate_on_container_move_assignment = std::false_type;
     using propagate_on_container_swap = std::false_type;
 
+public:
     Allocator() = default;
-    template<typename U>
-    Allocator(const Allocator<U>& rhs) noexcept {}
+    Allocator(const Allocator<T>& rhs) = default;
+    Allocator<T>& operator=(const Allocator<T>& rhs) = default;
+    Allocator(Allocator<T>&& rhs) = default;
+    Allocator<T>& operator=(Allocator<T>&& rhs) = default;
+    ~Allocator() noexcept = default;
 
-    [[nodiscard]] T* allocate(std::size_t count) const;
-    void deallocate(T* ptr, std::size_t count) const noexcept;
+    template<typename U>
+    constexpr Allocator(const Allocator<U>& rhs) noexcept {}
+
+    [[nodiscard]] constexpr T* allocate(std::size_t count) const;
+    constexpr void deallocate(T* ptr, std::size_t count) const noexcept;
 
     template<typename... Args>
-    void construct(T* ptr, Args&&... args) const;
-    void destroy(T* ptr) const noexcept;
-    std::size_t max_size() const noexcept;
+    constexpr void construct(T* ptr, Args&&... args) const;
+    constexpr void destroy(T* ptr) const noexcept;
+    [[nodiscard]] constexpr std::size_t max_size() const noexcept;
 };
 
 template<typename T>
-T* Allocator<T>::allocate(std::size_t count) const {
+constexpr T* Allocator<T>::allocate(std::size_t count) const {
     if (count == 0) {
         return nullptr;
     }
 
-    if (count > Allocator<T>::max_size()) {
+    if (count > max_size()) {
         throw std::bad_array_new_length();
     }
 
@@ -55,29 +72,28 @@ T* Allocator<T>::allocate(std::size_t count) const {
 }
 
 template<typename T>
-void Allocator<T>::deallocate(T* ptr, std::size_t count) const noexcept {
+constexpr void Allocator<T>::deallocate(T* ptr, std::size_t count) const noexcept {
     if (ptr != nullptr) {
         ::operator delete(ptr, (sizeof(T) * count));
-        ptr = nullptr;
     }
 }
 
 template<typename T>
 template<typename... Args>
-void Allocator<T>::construct(T* ptr, Args&&... args) const {
+constexpr void Allocator<T>::construct(T* ptr, Args&&... args) const {
     if (ptr != nullptr) {
         ::new (static_cast<void*>(ptr)) T(std::forward<Args>(args)...);
     }
 }
 
 template<typename T>
-void Allocator<T>::destroy(T* ptr) const noexcept {
+constexpr void Allocator<T>::destroy(T* ptr) const noexcept {
     if (ptr != nullptr) {
         ptr->~T();
     }
 }
 
 template<typename T>
-std::size_t Allocator<T>::max_size() const noexcept {
+constexpr std::size_t Allocator<T>::max_size() const noexcept {
     return std::numeric_limits<std::size_t>::max() / sizeof(T);
 }
--- include/StatefulAllocator.h
+++ include/StatefulAllocator.h
@@ -2,63 +2,79 @@
 #include <cstddef>
 #include <limits>
 #include <new>
+#include <type_traits>
+#include <utility>
 
 template<typename T>
 class StatefulAllocator;
 
-template<typename T>
-bool operator==(const StatefulAllocator<T>& lhs, const StatefulAllocator<T>& rhs) noexcept;
+template<typename T1, typename T2>
+constexpr bool operator==(const StatefulAllocator<T1>& lhs,
+                          const StatefulAllocator<T2>& rhs) noexcept;
 
-template<typename T>
-bool operator!=(const StatefulAllocator<T>& lhs, const StatefulAllocator<T>& rhs) noexcept;
+template<typename T1, typename T2>
+constexpr bool operator!=(const StatefulAllocator<T1>& lhs,
+                          const StatefulAllocator<T2>& rhs) noexcept;
 
 template<typename T>
 class StatefulAllocator
 {
 public:
     using value_type = T;
+
+    template<typename U>
+    struct rebind
+    {
+        using other = StatefulAllocator<U>;
+    };
+
     using is_always_equal = std::false_type;
     using propagate_on_container_copy_assignment = std::false_type;
     using propagate_on_container_move_assignment = std::false_type;
     using propagate_on_container_swap = std::false_type;
 
-public:
+public:    
     StatefulAllocator() = default;
-    explicit StatefulAllocator(std::size_t id);
-
     StatefulAllocator(const StatefulAllocator<T>& rhs) = default;
     StatefulAllocator<T>& operator=(const StatefulAllocator<T>& rhs) = default;
-
     StatefulAllocator(StatefulAllocator<T>&& rhs) = default;
     StatefulAllocator<T>& operator=(StatefulAllocator<T>&& rhs) = default;
+    ~StatefulAllocator() noexcept = default;
 
-    ~StatefulAllocator() = default;
+    explicit constexpr StatefulAllocator(std::size_t id) noexcept;
 
-    [[nodiscard]] T* allocate(std::size_t count) const;
-    void deallocate(T* ptr, std::size_t count) const noexcept;
+    template<typename U>
+    constexpr StatefulAllocator(const StatefulAllocator<U>& rhs) noexcept;
+
+    [[nodiscard]] constexpr T* allocate(std::size_t count) const;
+    constexpr void deallocate(T* ptr, std::size_t count) const noexcept;
 
     template<typename... Args>
-    void construct(T* ptr, Args&&... args) const;
-    void destroy(T* ptr) const noexcept;
-    [[nodiscard]] std::size_t max_size() const noexcept;
+    constexpr void construct(T* ptr, Args&&... args) const;
+    constexpr void destroy(T* ptr) const noexcept;
+    [[nodiscard]] constexpr std::size_t id() const noexcept;
+    [[nodiscard]] constexpr std::size_t max_size() const noexcept;
 
 private:
     std::size_t m_id{};
-
-    friend bool operator== <> (const StatefulAllocator<T>& lhs, const StatefulAllocator<T>& rhs) noexcept;
-    friend bool operator!= <> (const StatefulAllocator<T>& lhs, const StatefulAllocator<T>& rhs) noexcept;
 };
 
 template<typename T>
-StatefulAllocator<T>::StatefulAllocator(std::size_t id)
+constexpr StatefulAllocator<T>::StatefulAllocator(std::size_t id) noexcept
     : m_id(id) {}
+
 template<typename T>
-T* StatefulAllocator<T>::allocate(std::size_t count) const {
+template<typename U>
+constexpr StatefulAllocator<T>::StatefulAllocator(const StatefulAllocator<U>& rhs) noexcept
+    : m_id(rhs.id()) {}
+
+template<typename T>
+constexpr T* StatefulAllocator<T>::allocate(std::size_t count) const {
     if (count == 0) {
         return nullptr;
     }
 
-    if (count > StatefulAllocator<T>::max_size()) {
+    if (count > max_size()) {
         throw std::bad_array_new_length();
     }
 
@@ -66,39 +82,47 @@ T* StatefulAllocator<T>::allocate(std::size_t count) const {
 }
 
 template<typename T>
-void StatefulAllocator<T>::deallocate(T* ptr, std::size_t count) const noexcept {
+constexpr void StatefulAllocator<T>::deallocate(T* ptr, std::size_t count) const noexcept {
     if (ptr != nullptr) {
         ::operator delete(ptr, (count * sizeof(T)));
-        ptr = nullptr;
     }
 }
 
 template<typename T>
 template<typename... Args>
-void StatefulAllocator<T>::construct(T* ptr, Args&&... args) const {
+constexpr void StatefulAllocator<T>::construct(T* ptr, Args&&... args) const {
     if (ptr != nullptr) {
         ::new (static_cast<void*>(ptr)) T(std::forward<Args>(args)...);
     }
 }
 
 template<typename T>
-void StatefulAllocator<T>::destroy(T* ptr) const noexcept {
+constexpr void StatefulAllocator<T>::destroy(T* ptr) const noexcept {
     if (ptr != nullptr) {
         ptr->~T();
     }
 }
 
 template<typename T>
-std::size_t StatefulAllocator<T>::max_size() const noexcept {
+constexpr std::size_t StatefulAllocator<T>::id() const noexcept {
+    return m_id;
+}
+
+template<typename T>
+constexpr std::size_t StatefulAllocator<T>::max_size() const noexcept {
     return std::numeric_limits<std::size_t>::max() / sizeof(T);
 }
 
-template<typename T>
-bool operator==(const StatefulAllocator<T>& lhs, const StatefulAllocator<T> &rhs) noexcept {
-    return (lhs.m_id == rhs.m_id);
+template<typename T1, typename T2>
+constexpr bool operator==(const StatefulAllocator<T1>& lhs,
+                          const StatefulAllocator<T2>& rhs) noexcept
+{
+    return (lhs.id() == rhs.id());
 }
 
-template<typename T>
-bool operator!=(const StatefulAllocator<T>& lhs, const StatefulAllocator<T>& rhs) noexcept {
-    return (lhs.m_id != rhs.m_id);
+template<typename T1, typename T2>
+constexpr bool operator!=(const StatefulAllocator<T1>& lhs,
+                          const StatefulAllocator<T2>& rhs) noexcept
+{
+    return !(lhs == rhs);
 }
